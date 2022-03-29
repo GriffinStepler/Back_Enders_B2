0f007f8c7fefbdbaf776ce24ae29d35dd614e55e
Matt Clark <matthewgclark40@gmail.com>
2022-03-28 19:53:36 -0700

    Login Working with DB
    Login page that actively queries from the database and returns the account name to ./home

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -1,76 +1,19 @@
-// const mysql = require('mysql');
-// const express = require('express');
-// const session = require('express-session');
-// const path = require('path');
-
-// let connectionString = {
-//     host: "107.180.1.16",
-//     port: "3306",
-//     database: "sprog20222",
-//     user: "sprog20222",
-//     password: "sprog20222",
-// };
-
-// const app = express();
-
-// app.use(session({
-//     secret: 'secret',
-//     resave: true,
-//     saveUninitialized: true
-// }));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'static')));
-
-// // http://localhost:8080/
-// app.get('./', function(request, response) {
-//     // Render login template
-//     response.sendFile(path.join(__dirname + 'basicLogin.html'));
-// });
-
-// // http://localhost:8080/auth
-// app.post('./auth', function(request, response) {
-//     // Capture the input fields
-//     let username = request.body.username;
-//     let password = request.body.password;
-//     // Ensure the input fields exists and are not empty
-//     if (username && password) {
-//         // Execute SQL query that'll select the account from the database based on the specified username and password
-//         connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-//             // If there is an issue with the query, output the error
-//             if (error) throw error;
-//             // If the account exists
-//             if (results.length > 0) {
-//                 // Authenticate the user
-//                 request.session.loggedin = true;
-//                 request.session.username = username;
-//                 // Redirect to home page
-//                 response.redirect('/home');
-//             } else {
-//                 response.send('Incorrect Username and/or Password!');
-//             }
-//             response.end();
-//         });
-//     } else {
-//         response.send('Please enter Username and Password!');
-//         response.end();
-//     }
-// });
-
-// app.listen(8080);
-
 const mysql = require('mysql');
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
 
-let connection = {
+let connectionString = {
     host: "107.180.1.16",
     port: "3306",
     database: "sprog20222",
     user: "sprog20222",
     password: "sprog20222",
 };
+
+let connection = mysql.createConnection(connectionString)
+
+
 
 const app = express();
 
