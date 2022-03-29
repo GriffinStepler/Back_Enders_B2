7cd3492901dc050eda32fa74956f53315cf24d56
mattgc13 <matthewgclark40@gmail.com>
2022-03-29 02:00:28 -0700

    Working create login page (does not verify)
    Create login page works with db but I couldnt get the verification that an account with same email/username does not already exist

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -51,7 +51,7 @@ app.post('/auth', function(request, response) {
                 // Redirect to home page
                 response.redirect('/home');
             } else {
-                response.send('Incorrect Username and/or Password!');
+                response.send('Incorrect Username and/or Password! <br>  <p><a href="/">Login</a> <a href="/create">Create an Account</a></p>');
             }
             response.end();
         });
@@ -65,6 +65,7 @@ app.post('/auth', function(request, response) {
 app.get('/home', function(request, response) {
     // If the user is loggedin
     if (request.session.loggedin) {
+        console.log('successful login by', request.session.username)
         // Output username
         response.send('Welcome back, ' + request.session.username + '!');
     } else {
@@ -76,29 +77,63 @@ app.get('/home', function(request, response) {
 
 // http://localhost:3000/create
 app.get('/create', function(request, response) {
+    // Render create template
+    response.sendFile(path.join(__dirname + '/createAccount.html'));
+});
+
+// http://localhost:3000/createAuth
+//authorizes that the account is not already taken, and adds to db if so
+app.post('/createAuth', function(request, response) {
     //store form inputs into variables
     let username = request.body.username;
     let email = request.body.password;
     let password = request.body.password;
+
+    var acctInfo = [username, email, password]
     
     if (username && password && email) {
-        // Execute SQL query that'll select the account from the database based on the specified username and password
-        connection.query('SELECT * FROM accounts WHERE username = ? OR email = ?', [username, email], function(error, results, fields) {
-            // If there is an issue with the query, output the error
-            if (error) throw error;
-            // If the account exists
-            if (results.length = 0) {
-                // Authenticate the user
-                request.session.loggedin = true;
-                request.session.username = username;
-                // Redirect to login page
-                response.redirect('/login');
+
+        connection.query('INSERT INTO accounts (username, email, password) VALUES (?, ?, ?)', [username, email, password], function(error) {
+
+            // If there is an issue with the query, output the error, else redirect to login and say success
+            if (error) {
+
+                throw error
             } else {
-                response.send('Username or email already taken! <br>  <p><a href="/">Login</a> <a href="/create">Create an Account</a></p>');
-                
+                response.send('Successfully created new account!!!!')
+                // setTimeout(response.redirect('/'), 3000)
             }
-            response.end();
-        });
+            })
+
+
+
+        // Execute SQL query that'll select accounts with given username or email
+        // connection.query('SELECT * FROM accounts WHERE username = ? OR email = ?', [username, email], function(error, results, fields) {
+        //     // If there is an issue with the query, output the error
+        //     if (error) throw error;
+
+        //     // If the account does not exist
+        //     if (results.length = 0) {
+        //         // Add user to DB
+        //         connection.query('INSERT INTO account (username, email, password) VALUES ?', [username, email, password], function(error) {
+
+        //         // If there is an issue with the query, output the error, else redirect to login and say success
+        //         if (error) {
+
+        //             throw error
+        //         } else {
+        //             response.send('Successfully created new account!!!!')
+        //             setTimeout(response.redirect('/'), 3000)
+        //         }
+        //         })
+
+        //     } else {
+        //         console.log(results.length)
+        //         response.send('Username or email already taken! <br>  <p><a href="/">Login</a> <a href="/create">Create an Account</a></p>');
+        //         // response.end();
+        //     }
+        //     response.end();
+        // });
     } else {
         response.send('Please enter all required fields!');
         response.end();
